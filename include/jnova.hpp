22f3ba09791c01b65af47402a681321c60ba19df
Andrew Chambers <w3dojo@gmail.com>
2025-09-12 22:23:34 -0700

    Added custom exception class that takes advantage of the newish C++ feature source_location.

--- include/jnova.hpp
+++ include/jnova.hpp
@@ -3,68 +3,28 @@
 #ifndef JNOVA_HPP
 #define JNOVA_HPP
 
-#include <map>
-#include <set>
-#include <vector>
-
+#include <cstring>
 #include <exception>
 #include <format>
-#include <functional>
 #include <iostream>
 #include <source_location>
 #include <string>
-#include <variant>
 
 namespace JNOVA {
 
-struct Json;
-
-using Num_J    = std::variant<int, double>;
-using Str_J    = std::string;
-using Bool_J   = bool;
-using Null_J   = std::nullptr_t;
-using Arr_J    = std::vector<Json>;
-using Obj_J    = std::map<std::string, Json>;
-using JsonType = std::variant<Num_J, Str_J, Bool_J, Null_J, Arr_J, Obj_J>;
-
-struct Json : JsonType {
-  using JsonType::JsonType;
-
-  Json(const Arr_J &arr);
-  Json(Arr_J &&arr);
-  Json(const Obj_J &obj);
-  Json(Obj_J &&obj);
-  Json(std::initializer_list<std::pair<const std::string, Json>> init);
-};
-
-
-
-
-class Error : std::exception {
-  std::source_location srcLoc;
-  const char *func;
-  const char *file;
-  uint32_t line;
-
+class Ex : public std::exception {
+  std::string make_message() const;
+  std::source_location srcLoc_;
+  std::string reason_;
+  std::string message_;
 
 public:
-  inline Error(std::source_location srcLoc)
-  : srcLoc(srcLoc)
-  {
-    auto foo = srcLoc.function_name();
-  }
+  Ex(std::string reason,
+     const std::source_location &srcLoc = std::source_location::current());
 
-  inline ~Error()
-  {
-    delete this->func;
-    delete this->file;
-  }
-
-
-  inline const char *what() const noexcept override //
-  {
-    return "An error has occurred";
-  }
+  const char *what() const noexcept override;
 };
-};
+
+} // namespace JNOVA
+
 #endif
